650b8144f3d0ce833d19a50615ff93d940146b08
aliefadam <aliefadam6@gmail.com>
2024-03-12 14:26:41 +0700

    pengerjaan fitur stuktur

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -88,7 +88,9 @@ class StaffController extends Controller
     public function strukturPegawai()
     {
         return view('struktur_pegawai', [
-            "data_pengajuan" => DaftarPengajuan::getAll(),
+            "data_pegawai" => Karyawan::orderBy("jabatan_id", "ASC")->get(),
+            "jabatan_id" => auth()->user()->karyawan->jabatan_id,
+            "diatas_satu_level" => auth()->user()->karyawan->jabatan_id - 1,
             "title" => "Struktur Pegawai",
         ]);
     }
--- resources/views/struktur_pegawai.blade.php
+++ resources/views/struktur_pegawai.blade.php
@@ -3,17 +3,27 @@
 @section('content')
     <div class="bg-white shadow-xl rounded-lg px-5 py-10 w-full">
         <div class="scroll flex justify-center items-center flex-col gap-10">
-            <div class="direktur">
-                <div
-                    class="relative w-[200px] struktur-item bg-transparent border-2 border-indigo-600 rounded-lg p-5 flex flex-col items-center shadow-xl">
-                    <img src="{{ asset('imgs/profil.png') }}" class="w-[100px] drop-shadow-xl">
-                    <span class="text-indigo-600 font-medium mt-2 text-xl text-center">John Mayer</span>
-                    <span class="italic text-[13px] leading-none">Direktur</span>
-                    <div class="garis-menurun h-[20px] border border-indigo-600 absolute -bottom-[20px]"></div>
-                    <div class="garis-kesamping w-[123%] border border-indigo-600 absolute -bottom-[20px]"></div>
+            @for ($i = $diatas_satu_level; $i <= 5; $i++)
+                <div class="direktur flex gap-10">
+                    @php
+                        $pegawaiPerJabatan = $data_pegawai->where('jabatan_id', $jabatan_id - ($jabatan_id - $i));
+                        $jumlahPegawaiPerJabatan = $pegawaiPerJabatan->count();
+                    @endphp
+                    @foreach ($pegawaiPerJabatan as $pegawai)
+                        <div
+                            class="relative w-[200px] struktur-item bg-transparent border-2 border-indigo-600 {{ $pegawai->id == auth()->user()->id ? 'bg-gradient-to-r from-dbklik to-indigo-600' : '' }} rounded-lg p-5 flex flex-col items-center shadow-xl">
+                            <img src="{{ asset('imgs/profil.png') }}" class="w-[100px] drop-shadow-xl">
+                            <span
+                                class="text-indigo-600 {{ $pegawai->id == auth()->user()->id ? 'text-white' : '' }} font-medium mt-2 text-xl text-center">{{ $pegawai->nama_lengkap }}</span>
+                            <span
+                                class="italic text-[13px] leading-none {{ $pegawai->id == auth()->user()->id ? 'text-yellow-dbklik font-semibold' : '' }}">{{ $pegawai->subDivisi->nama_sub_divisi }}</span>
+                            {{-- <div class="garis-menurun h-[20px] border border-indigo-600 absolute -bottom-[20px]"></div>
+                            <div class="garis-kesamping w-[123%] border border-indigo-600 absolute -bottom-[20px]"></div> --}}
+                        </div>
+                    @endforeach
                 </div>
-            </div>
-            <div class="manager flex gap-10">
+            @endfor
+            {{-- <div class="manager flex gap-10">
                 <div
                     class="relative struktur-item w-[200px] bg-transparent border-2 border-indigo-600 rounded-lg p-5 flex flex-col items-center shadow-xl">
                     <img src="{{ asset('imgs/profil.png') }}" class="w-[100px] drop-shadow-xl">
@@ -28,7 +38,7 @@
                     <span class="text-yellow-dbklik font-semibold italic text-[13px] leading-none mt-1">Manager</span>
                     <div class="garis-naik h-[25px] border border-indigo-600 absolute -top-[25px]"></div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 @endsection
