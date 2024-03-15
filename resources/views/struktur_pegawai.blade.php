780f71278e82dd0c8bb932a49c79d4755cad4c5e
naldye <fionaldiandreanusmanik@gmail.com>
2024-03-15 12:53:49 +0700

    branch sementara

--- resources/views/struktur_pegawai.blade.php
+++ resources/views/struktur_pegawai.blade.php
@@ -9,6 +9,7 @@
                         $pegawaiPerJabatan = $data_pegawai->where('jabatan_id', $jabatan_id - ($jabatan_id - $i));
                         $jumlahPegawaiPerJabatan = $pegawaiPerJabatan->count();
                     @endphp
+                    <!-- <hr class="bg-indigo-600 opacity-100 absolute h-[2px] w-[calc(100%-200px)] -top-[25px] left-[50%] -translate-x-[50%]"> -->
                     @foreach ($pegawaiPerJabatan as $index => $pegawai)
                         <div
                             class="relative w-[200px] struktur-item bg-transparent border-2 border-indigo-600 {{ $pegawai->id == auth()->user()->id ? 'bg-gradient-to-r from-dbklik to-indigo-600' : '' }} rounded-lg p-5 flex flex-col items-center shadow-xl">
@@ -18,28 +19,19 @@
                             <span
                                 class="italic text-[13px] leading-none {{ $pegawai->id == auth()->user()->id ? 'text-yellow-dbklik font-semibold' : 'font-semibold' }}">{{ $pegawai->jabatan->nama_jabatan }}
                                 - {{ $pegawai->divisi->nama_divisi }}</span>
-                            <div class="garis-menurun h-[20px] border border-indigo-600 absolute -top-[20px]"></div>
-                            {{-- <div class="garis-kesamping w-[123%] border border-indigo-600 absolute -bottom-[20px]"></div> --}}
+                            @if ($i != $diatas_satu_level && $loop->last -1)
+                            
+                                <div class="garis-menurun h-5 w-[calc(100%+40px)] border-b-transparent border-2 border-indigo-600 absolute -top-[20px] left-[50%]"></div>
+                                <div class="garis-atas h-[22px] border border-indigo-600 absolute -top-[42px] translate-x-[120px]"></div>
+                                
+                            @endif
+                            <!-- <div class="garis-menurun h-[40px] border border-indigo-600 absolute -top-[40px]"></div> -->
+                            
                         </div>
                     @endforeach
                 </div>
             @endfor
-            {{-- <div class="manager flex gap-10">
-                <div
-                    class="relative struktur-item w-[200px] bg-transparent border-2 border-indigo-600 rounded-lg p-5 flex flex-col items-center shadow-xl">
-                    <img src="{{ asset('imgs/profil.png') }}" class="w-[100px] drop-shadow-xl">
-                    <span class="text-indigo-600 font-medium mt-3 text-xl text-center leading-none">John Mayer</span>
-                    <span class="text-black italic text-[13px] leading-none mt-1">Manager</span>
-                    <div class="garis-naik h-[25px] border border-indigo-600 absolute -top-[25px]"></div>
-                </div>
-                <div
-                    class="relative struktur-item w-[200px] bg-transparent border-2 border-indigo-600 bg-gradient-to-r from-dbklik to-indigo-600 rounded-lg p-5 flex flex-col items-center shadow-xl">
-                    <img src="{{ asset('imgs/profil.png') }}" class="w-[100px] drop-shadow-xl">
-                    <span class="text-white font-medium mt-3 text-xl text-center leading-none">John Mayer</span>
-                    <span class="text-yellow-dbklik font-semibold italic text-[13px] leading-none mt-1">Manager</span>
-                    <div class="garis-naik h-[25px] border border-indigo-600 absolute -top-[25px]"></div>
-                </div>
-            </div> --}}
+
         </div>
     </div>
 @endsection
